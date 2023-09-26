e0f522337e6d76303d2b5f59b041c7528c5a6098
Omonjon <omonjons077@gmail.com>
2023-09-26 17:41:07 +0500

    Partners section added and responsive done

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,6 +38,58 @@ const linkAction = () => {
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
+/*=============== SWIPER PARTNERS ===============*/
+let swiperPartners = new Swiper(".partners_swiper", {
+    grabCursor: true,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: true,
+    breakpoints: {
+        // when window width is >= 280px
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            // slideToClickedSlide: true,
+        },
+        400: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+            // slideToClickedSlide: true,
+        },
+        // when window width is >= 575px
+        575: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+            // slideToClickedSlide: true,
+        },
+        // when window width is >= 768px
+        768: {
+            slidesPerView: 3,
+            // spaceBetween: 0,
+            // slideToClickedSlide: true,
+        },
+        // when window width is >= 992px
+        992: {
+            slidesPerView: 4,
+            // spaceBetween: 0,
+            // slideToClickedSlide: true,
+        },
+        // when window width is >= 1200px
+        1200: {
+            slidesPerView: 4,
+            // spaceBetween: 0,
+            // slideToClickedSlide: true,
+        },
+        // when window width is >= 1440px
+        1440: {
+            slidesPerView: 5,
+            // spaceBetween: 0,
+            // slideToClickedSlide: true,
+        },
+    },
+    keyboard: true,
+});
+
 /*=============== SWIPER PROJECTS ===============*/
 let swiperProjects = new Swiper(".projects__container", {
     grabCursor: true,
